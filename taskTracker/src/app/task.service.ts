366b329c5e091c25d95ee623da40f27dbd955bf5
Kanak Sharma <kanaksharma412@gwu.edu>
2021-03-30 01:32:12 -0400

    taskTracker

---
+++ taskTracker/src/app/task.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { task } from './task/task.model';
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+
+  constructor(public http:HttpClient) { }
+
+  storeTask(task:any){
+    this.http.post("http://localhost:3000/tasks",task).subscribe(result=>console.log(result),error=>console.log(error));
+  }
+
+  loadTask():Observable<task[]>{
+    return this.http.get<task[]>("http://localhost:3000/tasks");
+  }
+}
