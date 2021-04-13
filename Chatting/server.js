717d26f91db00edda03cd290b274f4d93122b4c4
Kanak Sharma <kanaksharma412@gwu.edu>
2021-04-12 23:36:29 -0400

    Chatting

---
+++ Chatting/server.js
@@ -0,0 +1,21 @@
+let app = require("express")();
+let http = require("http").Server(app);
+let io = require("socket.io")(http);
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+})
+
+io.on("connection",(socket)=>{
+    console.log("Client connected to application");
+
+    socket.on("name",(name)=>{
+        console.log(name);
+    })
+
+    socket.on("message",(msg)=>{
+        console.log(msg);
+    })
+})
+
+http.listen(9090,()=>console.log("server running on port number 9090"));
